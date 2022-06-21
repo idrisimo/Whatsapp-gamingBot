fe076d7ad518f81dc3fe6565dcf06ad798eb4d45
idrisimo <idrissilva@hotmail.com>
2022-06-21 04:15:23 +0100

    swapped from using a reply to using lists to tally response. Will be tested later today

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const qrcode = require('qrcode-terminal');
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth, Buttons, List } = require('whatsapp-web.js');
 const { lfgxup } = require('./handlers');
 
 const waitFor = (time) => {
     return new Promise((resolve, reject) => {
-      setTimeout(() => resolve(true), time);
+        setTimeout(() => resolve(true), time);
     });
-  };
+};
 
 
 const client = new Client({
@@ -35,19 +35,24 @@ client.initialize();
 
 
 let playerList = []
-let startTime = ''
-let timerIsArmed = false
+const timer = 10 //In seconds
 
 client.on('message_create', async msg => {
     console.log('MESSAGE RECEIVED', msg);
+    if (msg.type === 'list_response') {
+        const contact = await msg.getContact();
+        const name = contact.pushname
+        playerList.push(name)
+        console.log("playerlist: ", playerList)
+    }
 
     if (msg.body === '!ping') {
         // Send a new message as a reply to the current one
         msg.reply('pong');
-
     } else if (msg.body === '!help') {
+        const commandsToSend = '*🤖--BAMBOO BOT--🤖*\nCommand List:\n-*!ping*--pong\n-*!groupinfo*--Provides information about group chat.\n-*!lfg*--Starts the team building process\n-*!x*--Used to signify you want to play this session.'
         let chat = await msg.getChat();
-        client.sendMessage(chat.id._serialized, '*--Bamboo Bot--*\nCommand List:\n-*!ping*--pong\n-*!groupinfo*--Provides information about group chat.\n-*!lfg*--Starts the team building process\n-*!x*--Used to signify you want to play this session.');
+        client.sendMessage(chat.id._serialized, '*🤖--BAMBOO BOT--🤖*\nCommand List:\n-*!ping*--pong\n-*!groupinfo*--Provides information about group chat.\n-*!teams*--Starts the team building process.');
 
     } else if (msg.body === '!groupinfo') {
         if (chat.isGroup) {
@@ -67,10 +72,8 @@ client.on('message_create', async msg => {
         // Send a new message to the same chat
         let chat = await msg.getChat();
         if (chat.isGroup) {
-            startTime = msg.timestamp
-            timerIsArmed = true
             client.sendMessage(chat.id._serialized, '🤖*--BAMBOO BOT--*🤖\nStarting New Session! \n\n *Reply to this message* with *!x* if your looking to play this session. The bot will do its best to split everyone into random teams.');
-            await waitFor(15000)
+            await waitFor(60000)
             await client.sendMessage(chat.id._serialized, lfgxup(playerList))
             playerList = []
         } else {
@@ -96,17 +99,20 @@ client.on('message_create', async msg => {
 
             msg.reply('🤖*--BAMBOO BOT--*🤖\nThis command can only be used in a group!');
         }
+    } else if (msg.body === '!buttons') {
+        let button = new Buttons('Button body', [{ body: 'bt1' }], 'title', 'footer');
+        client.sendMessage(msg.from, button);
+    } else if (msg.body === '!teams') {
+        let chat = await msg.getChat();
+        if (chat.isGroup) {
+            let sections = [{ title: '', rows: [{ title: 'Join Team', description: '' }] }];
+            let list = new List(`🤖*--BAMBOO BOT--*🤖\nStarting New Session! \n\n Click *Join Session* below, then click *Ready*. The bot will do its best to split everyone into random teams. Be sure to complete the above instructions within *${timer}* seconds, otherwise you will not be included in this session! \n\n _minimum of 5 people need to join (otherwise whats the point just play quads.)_`, 'Join Session', sections, 'Team Maker', 'footer');
+            client.sendMessage(chat.id._serialized, list);
+            await waitFor((timer * 1000))
+            client.sendMessage(msg.from, '🤖*--BAMBOO BOT--*🤖\nTimes up, generating teams...')
+            await waitFor(5000)
+            await client.sendMessage(chat.id._serialized, lfgxup(playerList))
+            playerList = []
+        }
     }
 })
-
-
-
-while(timerIsArmed) {
-    console.log(startTime, startTime + 10)
-    const currentTime = new Date()
-    const currentTimeEpoch = Math.round(currentTime.getTime() / 1000)
-    if(currentTimeEpoch == startTime + 10) {
-        console.log('timer up')
-        timerIsArmed = false
-    }
-}
